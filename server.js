78b78efb1619ba367adff56e289af98a7f4bcb0f
Ankesh Kumar Thakur <thakurankeshkumar50@gmail.com>
2025-08-30 11:13:14 +0530

    Cors added for all whebsite for testing

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: "https://authheaven.vercel.app", 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
+
+
+// app.use(cors({
+//   origin: "https://authhaven.vercel.app/", 
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// }));
 
 
 
